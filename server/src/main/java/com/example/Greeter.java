d54d7d5594b3263510fb90d67a7a64fd8ea72788
ernesrufin <48215806+ernesrufin@users.noreply.github.com>
2019-03-05 15:31:16 +0100

    Update Greeter.java

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -7,6 +7,11 @@ public class Greeter {
   public Greeter() {
 
   }
+  
+  /**
+  * @param nom de personne
+  * @return afficher le nom
+  */
 
   public final String greet(final String someone) {
     return String.format("Hello babacar, %s!", someone);
